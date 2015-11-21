984d17da69d5031668d08d75137822f009a59658
alexminnaar <Bailey12>
2015-11-20 20:45:07 -0800

    added linked list insert

--- linkedlist/LinkedList.py
+++ linkedlist/LinkedList.py
@@ -5,7 +5,7 @@ class LinkedList:
     def __init__(self, head):
         self.head = head
 
-    def insert(self, value):
+    def prepend(self, value):
         new_node = ListNode(value)
         new_node.next_node = self.head
         self.head = new_node
@@ -22,3 +22,18 @@ class LinkedList:
                 check_node = check_node.next_node
 
         return is_found
+
+    def insert(self, value, index):
+
+        insert_node = ListNode(value)
+
+        current_index = 0
+        current_node = self.head
+
+        while (current_index < index - 1):
+            current_index += 1
+            current_node = current_node.next_node
+            print current_node.value
+
+        insert_node.next_node = current_node.next_node
+        current_node.next_node = insert_node
--- tests/LinkedListTest.py
+++ tests/LinkedListTest.py
@@ -11,18 +11,26 @@ class LinkedListTest(unittest.TestCase):
         self.assertEqual(test_list.head.next_node, None, "head next node should be None")
 
 
-    def test_insert(self):
+    def test_prepend(self):
         test_list = LinkedList(ListNode(3))
         self.assertEqual(test_list.head.value, 3, "head value should be 3")
-        test_list.insert(2)
+        test_list.prepend(2)
         self.assertEqual(test_list.head.value, 2, "new head value should be 2")
 
 
     def test_search(self):
         test_list = LinkedList(ListNode(3))
-        test_list.insert(2)
-        test_list.insert(5)
+        test_list.prepend(2)
+        test_list.prepend(5)
         self.assertEqual(test_list.search(3), True, "3 is in this list")
+
+    def test_insert(self):
+        test_list = LinkedList(ListNode(3))
+        test_list.prepend(2)
+        test_list.prepend(4)
+        test_list.insert(1, 2)
+        self.assertEqual(test_list.head.value, 4, "1 is the head")
+        self.assertEqual(test_list.head.next_node.next_node.value, 1, "1 is the 3rd node value")
 
 
 if __name__ == '__main__':
